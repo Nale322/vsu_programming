f73d9475363e912518a197ce4a46b25177763e31
Nale322 <34818356+Nale322@users.noreply.github.com>
2019-09-29 18:07:03 +0300

    Delete 1.py

--- hw2/1.py
+++
@@ -1,7 +0,0 @@
-massiv = []
-x = input("Vvedi chislo esli ne zassal: ")
-while x:
-    massiv.append(x)
-    x = input("Vvedi chislo esli ne zassal: ")
-print(massiv)
-print('Nu huli ti enter nazhal??')
