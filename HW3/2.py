73053e4d38aa54365b00a38dce2236e70feac7ca
Nale322 <34818356+Nale322@users.noreply.github.com>
2019-10-19 12:14:24 +0300

    Add files via upload

--- HW3/2.py
+++ HW3/2.py
@@ -1,9 +1,10 @@
 a = int(input())
 if 0 < a < 3 or a == 12:
     print("Zima")
-if 2 < a < 6:
+elif 2 < a < 6:
     print("Vesna")
-if 5 < a < 9:
+elif 5 < a < 9:
     print("Leto")
-if 8 < a < 12:
+else:
+    8 < a < 12
     print("Osen")
