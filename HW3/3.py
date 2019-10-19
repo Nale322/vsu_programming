fdd379d31182aca78cd6a4eccc44a4fbdac12c90
Nale322 <34818356+Nale322@users.noreply.github.com>
2019-10-19 12:13:38 +0300

    Add files via upload

--- HW3/3.py
+++ HW3/3.py
@@ -1,9 +1,12 @@
-n = int(input('Vvedite chislo: '))
-f1 = f2 = 1
-i = 0
-while (i < n):
-    i += 1
-    s = f1 + f2
-    f1 = f2
-    f2 = s
-    print(s)
+def fib(n):
+    f1 = f2 = 1
+    i = 0
+    while (i < n):
+        i += 1
+        s = f1 + f2
+        f1 = f2
+        f2 = s
+        print(s)
+a = int(input())
+
+fib(a)
