db80873f676aa8e6bfddc180c30c53934b13beab
Nale322 <34818356+Nale322@users.noreply.github.com>
2019-10-20 13:25:55 +0300

    Update 6.py

--- HW3/6.py
+++ HW3/6.py
@@ -1,7 +1,7 @@
 import math
 def is_prime1(n):
     for i in range(2, int(math.sqrt(n) + 1)):
-        if n % i == 0: 
+        if not n % i: 
             return False
     return True
 a = int(input())
