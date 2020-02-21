a082535ee87fa45c0a62990be5043e13d7835429
Nale322 <34818356+Nale322@users.noreply.github.com>
2020-02-21 19:32:58 +0300

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,19 @@
+
+class Matrix:
+
+    def __init__(self, elem):
+        self.strok = None
+        self.stolb = None
+        self.elem = elem	
+    def __str__(self): 
+        f = ''
+        for x in self.matrix:
+            f += '\n'
+            for i in x:
+                f += f'{i}  '
+        return f
+
+
+b = [[1,2,3] , [4,5,6]]
+alo = Matrix(b)
+print(alo)
